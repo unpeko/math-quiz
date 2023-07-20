0e5ac7ab11ff02a22bde059e5814ca2dfcbbb3cc
unpeko <132242919+unpeko@users.noreply.github.com>
2023-07-20 23:54:43 +0000

    added statement generator and title print

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("this is a math quiz")
+# Functions go here...
+
+
+def statement_generator(statement, decoration):
+  # author: luka
+  # date: Jun 21, 2023
+  # generates a border around prints
+  # v1
+
+  sides = decoration
+
+  statement = "{} {} {}".format(sides, statement, sides)
+  top_bottom = decoration * len(statement)
+
+  print(top_bottom)
+  print(statement)
+  print(top_bottom)
+
+  return ""
+
+
+# Main routine goes here...
+
+# greeting
+# v1
+statement_generator("Math Mastermind", "!")
