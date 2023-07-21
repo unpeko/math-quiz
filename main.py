faf6c9fd9b29a55f6166cac2a65f3948202a5431
unpeko <132242919+unpeko@users.noreply.github.com>
2023-07-21 00:20:08 +0000

    implemented instructions and yes/no statement  did little bit of formatting

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def statement_generator(statement, decoration):
   # author: luka
   # date: Jun 21, 2023
   # generates a border around prints
-  # v1
+  # v1.1
 
-  sides = decoration
+  sides = decoration*8
 
   statement = "{} {} {}".format(sides, statement, sides)
   top_bottom = decoration * len(statement)
@@ -18,9 +18,35 @@ def statement_generator(statement, decoration):
 
   return ""
 
+def yes_no(question) :
+  # author: luka
+  # date: Jun 21, 2023
+  # guser input if they want to chose yes or no 
+  valid = False
+  while not valid:
+    response = input(question).lower()
 
+    if response == "yes" or response == "y":
+      response = "yes"
+      return response
+
+    elif response == "no" or response == "n":
+      response = "no"
+      return response
+
+    else: 
+        print("Please answer yes / no")
+      
 # Main routine goes here...
 
 # greeting
 # v1
 statement_generator("Math Mastermind", "!")
+print("  Welcome to the best math game")
+print()
+
+played_before = yes_no("Have you played the game before? ")
+
+if played_before == "no":
+  print("instructions")
+
